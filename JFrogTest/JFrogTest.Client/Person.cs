eb8facafea1b06063ea769c883c431c9ea804ccf
Jimmy Lee <jimmyleeatwork@gmail.com>
2020-09-09 22:25:04 -0700

    initial checkin

---
+++ JFrogTest/JFrogTest.Client/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace JFrogTest.Client
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ JFrogTest/JFrogTest.Client/PersonClient.cs
@@ -0,0 +1,16 @@
+﻿namespace JFrogTest.Client
+{
+    public class PersonClient
+    {
+        public Person GetPersonByName(string name)
+        {
+            if(name == "Jimmy Lee")
+                return new Person
+                {
+                    Name = "Jimmy Lee",
+                    Age = 99
+                };
+            return null;
+        }
+    }
+}
